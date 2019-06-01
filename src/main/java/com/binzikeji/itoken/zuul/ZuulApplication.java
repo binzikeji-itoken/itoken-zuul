5b8db794be7eb8ddbbcf5c12789beaaec3feb9f7
ShuaiBin <3166688833@qq.com>
2019-06-01 23:56:18 +0800

    init

---
+++ src/main/java/com/binzikeji/itoken/zuul/ZuulApplication.java
@@ -0,0 +1,21 @@
+package com.binzikeji.itoken.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Description
+ * @Author Bin
+ * @Date 2019/4/3 11:23
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
